a85297f07aaf970f18503f754a1128031fbb90ea
Cam <cameronevans1993@gmail.com>
2024-04-25 16:10:01 +1200

    added comments

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -6,41 +6,42 @@ import matplotlib.pyplot as plt
 
 class Perceptron:
     def __init__(self, num_inputs, rate=0.01):
-        # initialise the weight and rate
+        # initialise the weights randomly and set the learnign rate
         self.weights = np.random.rand(num_inputs + 1)
         self.learning_rate = rate
-        self.errors = []
 
-    # Define the linear layer
+    # Define the linear layer, calculate linear combinations of input and weights
     def linear(self, inputs):
         Z = inputs @ self.weights[1:].T + self.weights[0]
         return Z
     
+    # Function that returns 1 if z >=0, else return 0
     def heaviside_step_fn(self, z):
         return np.where(z >= 0, 1, 0)
 
-    # predict 
+    # predict function, takes the linear function, applies the step function
     def predict(self, inputs):
             Z = self.linear(inputs)
             return self.heaviside_step_fn(Z)
 
+    # Function to calculate the difference between target and prediction
     def loss(self, prediction, target):
         return target - prediction
     
+    # function to train the dataset
     def fit(self, X, y, max_epochs=100):
         for epoch in range(max_epochs):
             converged = True
             misclassified = 0
             for input_row, target_row in zip(X, y):
-                prediction = self.predict(input_row)
-                error = self.loss(prediction, target_row)
+                pred = self.predict(input_row)
+                err = self.loss(pred, target_row)
                 input_row = input_row.astype(float)
-                self.weights[1:] += self.learning_rate * error * input_row
-                self.weights[0] += self.learning_rate * error
-                misclassified += np.abs(error)
-                if error != 0:
+                self.weights[1:] += self.learning_rate * err * input_row
+                self.weights[0] += self.learning_rate * err
+                misclassified += np.abs(err)
+                if err != 0:
                     converged = False
-                self.errors.append(misclassified)
             if converged:
                 print(f"Converged after {epoch + 1} epochs.")
                 break
@@ -56,7 +57,7 @@ if __name__ == "__main__":
 
     data = pd.read_csv(file, sep=' ')
 
-    # split data into train and test 
+    # split data into train and test, 80% of the data for training, 20% for testing
     train = data.sample(frac=0.8)
     test = data.drop(train.index)
     train = train.to_numpy()
@@ -68,17 +69,18 @@ if __name__ == "__main__":
     trainY = np.where(trainY == 'g', 1,0)
     testY = np.where(testY == 'g', 1,0)
 
-    np.random.seed(23)
+    np.random.seed(29)
 
     perceptron = Perceptron(len(trainX[1]))
 
     perceptron.fit(trainX, trainY)
 
-    # Model Evaluation
+    # Trianing model evaluation
     pred_train = perceptron.predict(trainX)
     accuracy_train = np.mean(pred_train == trainY)
     print("Train Accuracy:", accuracy_train)
 
+    # Test model evaluation
     pred_test = perceptron.predict(testX)
     accuracy_test = np.mean(pred_test == testY)
     print("Test Accuracy:", accuracy_test)
@@ -86,8 +88,6 @@ if __name__ == "__main__":
     # Print final weights
     print("Final Weights:", perceptron.weights)
 
-    num_features = len(perceptron.weights)
-    # Create an array of feature indices
-    feature_indices = np.arange(num_features)
+   
 
     
